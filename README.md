e32f0a531f3c8331e6f95f89de7e33b91e90f182
shan224 <shan224@gmail.com>
2017-02-18 12:48:05 -0800

    This is the new testing branch with the new folder structure and passport. Branch off of this one for your own testing

---
+++ README.md
@@ -0,0 +1,11 @@
+Marketplace
+==============
+
+A basic Node.js application which will use [Passport](http://passportjs.org/) as the authentication middleware for authenticating against a locally configured Mongo backend. Users can search for or post their offered services.
+
+Steps to run the app
+=====================
+* After cloning the repo, install the dependencies by running **npm install**
+* To start the server, run **npm start** on the base directory
+
+
--- app.js
+++ app.js
@@ -4,43 +4,92 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var db_server  = process.env.DB_ENV || 'primary';
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
+/********************** DATABASE **************************/
+var dbConfig = require('./db');
+var mongoose = require('mongoose');
+
+// Connect to DB
+db = mongoose.connect(dbConfig.url);
+// If the connection throws an error
+mongoose.connection.on("error", function(err) {
+  console.error('Failed to connect to DB ' + db_server + ' on startup ', err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection to DB :' + db_server + ' disconnected');
+});
+
+var gracefulExit = function() { 
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection with DB :' + db_server + ' is disconnected through app termination');
+    db.disconnect();
+    process.exit(0);
+  });
+}
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
+
+/***********************************************************************/
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico')); (Uncomment this when we have a favicon)
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+// TODO - Why Do we need this key ?
+app.use(expressSession({secret: 'mySecretKey',
+                        name: 'cookie_name',
+                        proxy: true,
+                        resave: true,
+                        saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// catch 404 and forward to error handler
+ // Using the flash middleware provided by connect-flash to store messages in session
+ // and displaying in templates
+var flash = require('connect-flash');
+app.use(flash());
+
+// Initialize Passport
+var initPassport = require('./passport/init');
+initPassport(passport);
+
+var routes = require('./routes/index')(passport);
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
 module.exports = app;
