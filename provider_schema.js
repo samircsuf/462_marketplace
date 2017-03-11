65fc3403e0fde2c619f1b1b5459542ace11ae3fc
shan224 <shan224@gmail.com>
2017-03-11 11:40:25 -0800

    Checkout Samirs profile schema

---
+++ provider_schema.js
@@ -0,0 +1,167 @@
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/passport';
+
+mongoose.connect(dbURI);
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+
+var Schema = mongoose.Schema;
+
+var providerSchema = new Schema({
+	_id: {type: String, required: true}, //getNextSequence("sid"), //ObjectID('AA1111')	
+	first_name: {type: String, required: true, uppercase: true},
+	last_name: {type: String, required: true, uppercase: true},
+	username: {type: String, min: 6, max: 35, required: true, lowercase: true, index: { unique: true }},
+	password: {type: String, min: 8, max: 16, select: true},//makes password accessible by default so that we can fetch it from db.Normally, it should be false
+	organization: {type: String, required:false},
+	contractorID:{type: String, required:true},
+	contact:{//one service provider will have only one group of 'contact' information 
+		phone:{type: Number, required: true},
+		email: {type: String, match: /\S+@\S+\.\S+/, required: true},
+		zip: {type: Number, required: true},
+		state: {type: String, required: true}
+    },
+	radius: {type: Number, required: true},
+	comments: [{//under one service provider, multiple comments can be inserted which are to be stored in an array
+        subuser: {type: String, required: true},
+		comment: {type: String, required: true},
+		rating: {type: Number, required: false}
+    }],
+	services : [{//one service provider can register to multiple services in a site
+        name: {type: String, required: true},
+        rate: {type: Number, required: true}
+    }],//{type: Array , "default" : []},
+	created: {type: Date,  required: true, default: Date.now},
+	modified: {type: Date,  required: true, default: Date.now},
+}, {strict: true});// 'true' indicates that values not defined in schema arent saved into the db
+//we would use population to refer between collections.
+
+var Provider = mongoose.model('Provider', providerSchema);
+
+//auto increment _id
+/*
+db.Provider.insert(
+   {
+      _id: "sid",
+      seq: AAB10000
+   }
+);
+
+function getNextSequence (name) {
+   var ret = db.Provider.findAndModify(
+          {
+            query: { _id: name },
+            update: { $inc: { seq: 1 } },
+            new: true
+          }
+   );
+
+   return ret.seq;
+}
+*/
+var providerRecords = [
+    {
+	_id: 'AAB10001', //getNextSequence("sid"), //ObjectID('AA1111')	
+	first_name: 'John',
+	last_name: 'Lowes',
+    username: 'jlowes',
+	organization: 'XYZ Contractors',
+	contractorID: '1234567',
+	contact:{
+		phone:7148012345,
+		email: 'xyz@gmail.com',
+		zip: 92831,
+		state: 'CA'
+    },
+	radius: 8,
+    comments:[{
+        subuser: 'xxxxx',
+		comment: 'Best ever Service',
+		rating: 4.5
+    },
+    {   
+        subuser: 'yyyy',
+		comment: 'Great Service',
+		rating: 4.0
+    }],
+	services:[{
+        name:'Math Tutor', 
+        rate: 15
+        },
+        {
+        name:'Science Tutor',
+        rate: 20
+        }
+        //{name:'Coding Assignment', rate: 25},
+        //{name:'Code Troubleshooting', rate: 30}
+    ],
+	created: Date.now(function(){return new Date().toISOString();}),
+	modified: Date.now(function(){return new Date().toISOString();})
+},{
+	_id: 'AAB10002',//getNextSequence("sid"), //ObjectID('AA1111')	
+	first_name: 'Eric',
+	last_name: 'Meyers',
+    username: 'ericmc',
+	organization: 'ABC Contractors',
+	contractorID: '1234567',
+	contact:{
+		phone:7162319045,
+		email: 'meyerec@gmail.com',
+		zip: 92820,
+		state: 'CA'
+    },
+	radius: 8,
+    comments:[{
+        subuser: 'xxxxx',
+		comment: 'Best ever Service',
+		rating: 4.5
+    },
+    {   
+        subuser: 'yyyy',
+		comment: 'Great Service',
+		rating: 4.0
+    }],
+	services:[{
+        name:'Coding Tutor', 
+        rate: 15
+        },
+        {
+        name:'Coding Assignment',
+        rate: 20
+        }
+        //{name:'Coding Assignment', rate: 25},
+        //{name:'Code Troubleshooting', rate: 30}
+    ],
+	created: Date.now(function(){return new Date().toISOString();}),
+	modified: Date.now(function(){return new Date().toISOString();})
+}
+];
+
+//Save multiple record
+Provider.collection.insert(providerRecords, function (err, docs) {
+    if (err)
+        console.log('error:', err);
+    else
+        console.log('Documents insert result:', docs);
+});
+
+function dc (msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+}
+
+//close the connection
+mongoose.connection.close();
