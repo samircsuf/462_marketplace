f97d02509456eb1c0579a6a4e161dfbec86e85d4
shan224 <shan224@gmail.com>
2017-04-15 09:41:06 -0700

    changed profile route

--- routes/index.js
+++ routes/index.js
@@ -66,7 +66,7 @@ module.exports = function(passport){
 
 	/* GET Home Page */
 	router.get('/home', isAuthenticated, function(req, res){
-		res.render('profile', { user: req.user });
+		res.render('profile_page', { user: req.user });
 	});
 
 	/* Handle Logout */
