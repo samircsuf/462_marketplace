75ad83ec27ebe7cb7f3cc553ff56f1af1f823f82
Daniel Castro <castrodan93@gmail.com>
2017-04-16 03:44:38 -0700

    I was able to update the fields from the form. Now all i need to do is have checks to see if there is any empty fields. Right now, its replacing current data with the blank field

---
+++ middleware/update.js
@@ -0,0 +1,24 @@
+var Provider = require('../models/provider.js');
+
+var export_data = null;
+
+module.exports = function(search_param,res){
+  var re = new RegExp(search_param, 'i');
+    Provider.find().or([{'_id' : search_param }, { 'services.name' : {$regex: re}}, { 'organization' : {$regex: re}}, { 'username' : {$regex: re}}, { 'first_name' : {$regex: re}}, { 'last_name': {$regex: re}}]).
+    exec(function (err, data) {
+    if(err)
+        console.log(err);
+    else{
+        console.log("hello");
+        console.log(data);
+        // console.log('Found documents for services1: \n', data.split(','));
+        // console.log(JSON.stringify(data,null,2));
+        var temp = [];
+        var conSize = 3;
+        for(var i = 0; i < data.length; i += conSize){
+          temp.push(data.slice(i, i + conSize));
+        }
+         res.render('profile_page',{ layout : 'layout', json: temp });
+       }
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var query = require('../middleware/query');
 var profile_query = require('../middleware/profile_query');
-var mongo = require('mongodb');
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 var dbConfig = require('../db');
 var Provider = require('../models/provider')
@@ -34,16 +35,30 @@ module.exports = function(passport){
 
 	});
 
-	router.post('/provider',function(req,res){
-
-	});
 	router.get('/profile_page/:id', function(req,res){
-
 		profile_query(req.params.id,res);
 	});
 
-  router.get('/provider', function(req,res){
-      res.render('provider',{message: req.flash('message')});
+	router.post('/provider/:id',function(req,res){
+		var data = {
+			first_name : req.body.firstName,
+			last_name : req.body.lastName,
+			organization: req.body.org,
+			"contact.phone" : req.body.tel,
+			"contact.email" : req.body.email,
+			"contact.state" : req.body.state,
+			"contact.zip" : req.body.zip,
+			radius : req.body.radius
+		};
+		var userID = req.params.id;
+			Provider.updateOne({"_id": objectId(userID)}, {$set: data}, function(err, results){
+			console.log("Updated Item.");
+		});
+		profile_query(req.params.id,res);
+	});
+
+  router.get('/provider/:id', function(req,res){
+      res.render('provider',{data : req.params.id});
   });
 
 	router.get('/login', function(req, res) {
